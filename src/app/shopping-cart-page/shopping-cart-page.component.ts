365b7af47d02b9870fd864a5b45e49fcad2633d0
Keyunzou <ya.10200916@gmail.com>
2024-06-19 02:51:11 +0800

    feat(order): 刪除購物車項目

--- src/app/shopping-cart-page/shopping-cart-page.component.ts
+++ src/app/shopping-cart-page/shopping-cart-page.component.ts
@@ -41,4 +41,9 @@ export class ShoppingCartPageComponent implements OnInit {
       this.details.push(control);
     }
   }
+
+  onDelete(index: number, id: number | undefined): void {
+    this.details.removeAt(index);
+    this.shoppingCartService.deleteProduct(id!);
+  }
 }
