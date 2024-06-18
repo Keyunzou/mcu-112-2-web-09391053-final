55f45e70646a905d57b7915f3a8f4882ad17d30c
Keyunzou <ya.10200916@gmail.com>
2024-06-19 01:30:28 +0800

    feat(nav): 導覽列切換實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'shopping-cart', component: ShoppingCartPageComponent },
+  { path: 'login', component: LoginPageComponent },
+];
