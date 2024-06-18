fab8fb42abe5700d4ed64c1889f0e00600dd055b
Keyunzou <ya.10200916@gmail.com>
2024-06-19 01:33:44 +0800

    feat(product-card): 產品卡片元件實作新增

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  @HostBinding('class')
+  class = 'product-card';
+}
