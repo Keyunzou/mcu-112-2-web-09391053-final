1c333e578f589b84ce799158d03dd105bc3e124c
Keyunzou <ya.10200916@gmail.com>
2024-06-19 02:40:52 +0800

    feat(login): 移除登入按鈕

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
 
@@ -7,5 +6,4 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
   { path: 'shopping-cart', component: ShoppingCartPageComponent },
-  { path: 'login', component: LoginPageComponent },
 ];
